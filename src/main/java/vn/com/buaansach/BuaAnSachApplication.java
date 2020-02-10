a3a0f1af0e68d257a630619a8fc5f3e3120ee385
ductranvp <ductranvp@gmail.com>
2020-02-10 10:25:12 +0700

    init

---
+++ src/main/java/vn/com/buaansach/BuaAnSachApplication.java
@@ -0,0 +1,13 @@
+package vn.com.buaansach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuaAnSachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuaAnSachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/com/buaansach/BuaAnSachApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.buaansach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuaAnSachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
