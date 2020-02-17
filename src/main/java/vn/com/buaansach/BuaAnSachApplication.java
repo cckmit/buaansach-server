5c9c289432367583e9271fae48303e51bf510959
ductranvp <ductranvp@gmail.com>
2020-02-17 10:47:25 +0700

    config audit event

--- src/main/java/vn/com/buaansach/BuaAnSachApplication.java
+++ src/main/java/vn/com/buaansach/BuaAnSachApplication.java
@@ -1,13 +1,86 @@
 package vn.com.buaansach;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import vn.com.buaansach.service.util.Constants;
+
+import javax.annotation.PostConstruct;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
 
 @SpringBootApplication
+@EntityScan(
+        basePackageClasses = {
+                BuaAnSachApplication.class,
+                Jsr310JpaConverters.class
+        }
+)
 public class BuaAnSachApplication {
+    private static final Logger log = LoggerFactory.getLogger(BuaAnSachApplication.class);
+    private final Environment env;
 
-	public static void main(String[] args) {
-		SpringApplication.run(BuaAnSachApplication.class, args);
-	}
+    public BuaAnSachApplication(Environment env) {
+        this.env = env;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BuaAnSachApplication.class);
+        HashMap<String, Object> defProperties = new HashMap<>();
+        defProperties.put("spring.profiles.default", Constants.SPRING_PROFILE_DEVELOPMENT);
+        app.setDefaultProperties(defProperties);
+        Environment env = app.run(args).getEnvironment();
+        logApplicationStartup(env);
+    }
+
+    private static void logApplicationStartup(Environment env) {
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isBlank(contextPath)) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}{}\n\t" +
+                        "External: \t{}://{}:{}{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.getActiveProfiles());
+    }
+
+    @PostConstruct
+    void init() {
+        Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+        if (activeProfiles.contains(Constants.SPRING_PROFILE_DEVELOPMENT)
+                && activeProfiles.contains(Constants.SPRING_PROFILE_PRODUCTION)) {
+            log.error("You have misconfigured your application! It should not run " +
+                    "with both the 'dev' and 'prod' profiles at the same time.");
+        }
+    }
 
 }
--- src/main/java/vn/com/buaansach/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/vn/com/buaansach/security/jwt/JwtAuthenticationEntryPoint.java
@@ -13,13 +13,13 @@ import java.io.IOException;
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
+    private static final Logger log = LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
 
     @Override
     public void commence(HttpServletRequest httpServletRequest,
                          HttpServletResponse httpServletResponse,
                          AuthenticationException e) throws IOException {
-        LOGGER.error("Responding with unauthorized error. Message - {}", e.getMessage());
+        log.error("Responding with unauthorized error. Message - {}", e.getMessage());
         httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
     }
 }
