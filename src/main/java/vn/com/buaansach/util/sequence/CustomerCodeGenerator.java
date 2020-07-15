1e93c062779dadb790eabe78537f5165a76df1e6
ductranvp <ductranvp@gmail.com>
2020-07-15 09:26:10 +0700

    update code generator

--- src/main/java/vn/com/buaansach/util/sequence/CustomerCodeGenerator.java
+++ src/main/java/vn/com/buaansach/util/sequence/CustomerCodeGenerator.java
@@ -3,8 +3,8 @@ package vn.com.buaansach.util.sequence;
 import java.time.Instant;
 
 public final class CustomerCodeGenerator {
-    /*2020-01-01 00:00:00 UTC+0*/
-    private static final long CUSTOM_EPOCH = 1577836800000L;
+    /*Sunday, 21 June 2020 00:00:00 UTC+0*/
+    private static final long CUSTOM_EPOCH = 1592697600000L;
 
     public static String generate() {
         return (Instant.now().toEpochMilli() - CUSTOM_EPOCH) + "";
--- src/main/java/vn/com/buaansach/util/sequence/OrderCodeGenerator.java
+++ src/main/java/vn/com/buaansach/util/sequence/OrderCodeGenerator.java
@@ -5,8 +5,8 @@ import vn.com.buaansach.web.admin.service.AdminCodeService;
 import java.time.Instant;
 
 public final class OrderCodeGenerator {
-    /*2020-01-01 00:00:00 UTC+0*/
-    private static final long CUSTOM_EPOCH = 1577836800000L;
+    /*Sunday, 21 June 2020 00:00:00 UTC+0*/
+    private static final long CUSTOM_EPOCH = 1592697600000L;
 
     public static String generate(String storeCode) {
         storeCode = storeCode.replace(AdminCodeService.STORE_CODE_PREFIX, "");
