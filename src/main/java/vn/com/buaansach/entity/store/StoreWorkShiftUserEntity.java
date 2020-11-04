c675b917738b399cf391b145ed7c3b78bdecbf3f
ductranvp <ductranvp@gmail.com>
2020-11-04 09:10:11 +0700

    update work shift db

--- src/main/java/vn/com/buaansach/entity/store/StoreWorkShiftUserEntity.java
+++ src/main/java/vn/com/buaansach/entity/store/StoreWorkShiftUserEntity.java
@@ -24,6 +24,14 @@ public class StoreWorkShiftUserEntity implements Serializable {
     @Column(name = "work_day")
     private String workDay;
 
+    private String monday;
+    private String tuesday;
+    private String wednesday;
+    private String thursday;
+    private String friday;
+    private String saturday;
+    private String sunday;
+
     /**
      * FK
      */
